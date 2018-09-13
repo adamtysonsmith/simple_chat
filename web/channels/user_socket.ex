955dd6eb3e20415810c0383320916f3f8526940f
Adam <atsmith227@gmail.com>
2018-09-13 15:40:37 -0600

    secret key

--- web/channels/user_socket.ex
+++ web/channels/user_socket.ex
@@ -9,7 +9,7 @@ defmodule SimpleChat.UserSocket do
   # this is where you want auth logic
   def connect(%{"token" => token}, socket) do
     # max age is one day
-    case Phoenix.Token.verify(socket, "secretkey", token, max_age: 86400) do
+    case Phoenix.Token.verify(socket, System.get_env("SOCKET_SECRET"), token, max_age: 86400) do
       {:ok, user_id} -> {:ok, assign(socket, :user_id, user_id)}
       {:error, _error} -> :error
     end
